dbc2ea62d8769047e82f406bea0fd0578bd643a5
ho-taek <kht072623@naver.com>
2025-02-18 19:10:00 +0900

    chore : 프로젝트 세팅

---
+++ src/main/java/signal/sailbot/SailbotApplication.java
@@ -0,0 +1,13 @@
+package signal.sailbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SailbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/signal/sailbot/SailbotApplicationTests.java
@@ -0,0 +1,13 @@
+package signal.sailbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
